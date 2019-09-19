95ac119e56b40b5085ad3e32cdc47cfad2225c0a
Rad <chihaby@gmail.com>
2019-09-19 07:37:06 -0700

    setting up user routes & models

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
-
-const items = require('./routes/api/items');
 
 const app = express();
 
 //Bodyparser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
 
 // Connect to Mongo
 mongoose
-    .connect(db)
+    .connect(db, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
 // Use routes
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
 
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
