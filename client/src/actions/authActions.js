0e10b620c57bb102f2ec127f3895dec857a92061
Rad <chihaby@gmail.com>
2019-09-22 21:09:54 -0700

    register model complete

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -31,10 +31,41 @@ export const loadUser = () => (dispatch, getState) => {
                 type: AUTH_ERROR
             });
         });
-
-    axios.get("/api/auth/user");
 };
 
+// Register User
+export const register = ({ name, email, password }) => dispatch => {
+    // Headers
+    const config = {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    // Request body
+    const body = JSON.stringify({ name, email, password });
+
+    axios
+        .post("/api/users", body, config)
+        .then(res =>
+            dispatch({
+                type: REGISTER_SUCCESS,
+                payload: res.data
+            })
+        )
+        .catch(err => {
+            dispatch(
+                returnErrors(
+                    err.response.data,
+                    err.response.status,
+                    " REGISTER_FAIL"
+                )
+            );
+            dispatch({
+                type: REGISTER_FAIL
+            });
+        });
+};
 // Setup config/headers and token
 export const tokenConfig = getState => {
     // Get token from local storage
