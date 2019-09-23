3cf10139c3b0c2be7ce3797a3e00b74872e42f19
Rad <chihaby@gmail.com>
2019-09-23 08:56:01 -0700

    adds login component

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -5,8 +5,8 @@ import {
     USER_LOADED,
     USER_LOADING,
     AUTH_ERROR,
-    // LOGIN_SUCCESS,
-    // LOGIN_FAIL,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
     LOGOUT_SUCCESS,
     REGISTER_SUCCESS,
     REGISTER_FAIL
@@ -67,7 +67,41 @@ export const register = ({ name, email, password }) => dispatch => {
         });
 };
 
-// logout user
+// Login User
+export const login = ({ email, password }) => dispatch => {
+    // Headers
+    const config = {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    // Request body
+    const body = JSON.stringify({ email, password });
+
+    axios
+        .post("/api/auth", body, config)
+        .then(res =>
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: res.data
+            })
+        )
+        .catch(err => {
+            dispatch(
+                returnErrors(
+                    err.response.data,
+                    err.response.status,
+                    " LOGIN_FAIL"
+                )
+            );
+            dispatch({
+                type: LOGIN_FAIL
+            });
+        });
+};
+
+// Logout user
 export const logout  = () => {
     return {
     type: LOGOUT_SUCCESS
@@ -88,7 +122,7 @@ export const tokenConfig = getState => {
     };
 
     // If token, add to headers
-    if (token) {
+    if (token) { 
         config.headers["x-auth-token"] = token;
     }
     return config;
