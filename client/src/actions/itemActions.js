8c6f4aac3e6f2cbe9d603a3c228587c67f9b2dc2
Rad <chihaby@gmail.com>
2019-09-23 21:21:43 -0700

    login/logout error

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,42 +1,52 @@
-import axios from 'axios';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING  } from './types';
-// import { set } from 'mongoose';
+import axios from "axios";
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from "./types";
+import { tokenConfig } from "./authActions";
+import { returnErrors } from "./errorActions";
 
-export const getItems = () =>dispatch => {
+export const getItems = () => dispatch => {
     dispatch(setItemsLoading());
-    // proxy will handle http:// ...
     axios
-        .get('/api/items')
-        .then(res=> 
+        .get("/api/items")
+        .then(res =>
             dispatch({
                 type: GET_ITEMS,
                 payload: res.data
             })
         )
+        .catch(err =>
+            dispatch(returnErrors(err.response.data, err.response.status))
+        );
 };
 
-export const addItem = item => dispatch => {
-axios
-    // No need to include http:// the proxy in package json is set to handle that
-    .post('/api/items', item)
-    .then(res => 
-        dispatch({
-            type: ADD_ITEM,
-            payload: res.data
-        }))
-};
-
-export const deleteItem = id => dispatch => {
+export const addItem = item => (dispatch, getState) => {
     axios
-        .delete(`/api/items/${id}`).then(res =>
+        .post("/api/items", item, tokenConfig(getState))
+        .then(res =>
+            dispatch({
+                type: ADD_ITEM,
+                payload: res.data
+            })
+        )
+        .catch(err =>
+            dispatch(returnErrors(err.response.data, err.response.status))
+        );
+};
+
+export const deleteItem = id => (dispatch, getState) => {
+    axios
+        .delete(`/api/items/${id}`, tokenConfig(getState))
+        .then(res =>
             dispatch({
                 type: DELETE_ITEM,
                 payload: id
-            }))
+            })
+        )
+        .catch(err =>
+            dispatch(returnErrors(err.response.data, err.response.status))
+        );
 };
 
-
-export const setItemsLoading = ()  => {
+export const setItemsLoading = () => {
     return {
         type: ITEMS_LOADING
     };
--- client/src/components/auth/Logout.js
+++ client/src/components/auth/Logout.js
@@ -1,14 +1,18 @@
-import React, { Component, Fragment } from 'react'; 
-import { NavLink } from 'reactstrap';
-import { connect } from 'react-redux';
-import { logout } from '../../actions/authActions';
-// import PropType from 'prop-types';
+import React, { Component, Fragment } from "react";
+import { NavLink } from "reactstrap";
+import { connect } from "react-redux";
+import { logout } from "../../actions/authActions";
+import PropTypes from "prop-types";
 
-export class Logout extends Component  {
-    render () {
+export class Logout extends Component {
+    static propTypes = {
+        logout: PropTypes.func.isRequired
+    };
+
+    render() {
         return (
             <Fragment>
-                <NavLink onClick={this.props.logout} href='#'>
+                <NavLink onClick={this.props.logout} href="#">
                     Logout
                 </NavLink>
             </Fragment>
@@ -16,4 +20,7 @@ export class Logout extends Component  {
     }
 }
 
-export default connect(null, { logout })(Logout) ;
+export default connect(
+    null,
+    { logout }
+)(Logout);
